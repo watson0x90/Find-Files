1444378eedaafda8c982064888c8ad4dcaed92aa
watson0x90 <watson0x90@gmail.com>
2015-05-22 09:17:00 -0600

    LICENSE And Terms Update
    Updated to include LICENSE and Terms

--- Find-Files.ps1
+++ Find-Files.ps1
@@ -1,13 +1,25 @@
 <#
+
+# All credit to the original author(s): Ryan Watson (Watson0x90)
+#
+# Find-Files.ps1 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Find-Files.ps1 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Find-Files.ps1.  If not, see <http://www.gnu.org/licenses/>.
+
 .SYNOPSIS
 This script is used to find intersting files
 
-
-
-
-
 Function: Find-Files
-Author: Watsonox90
+Author: Ryan Watson (Watsonox90)
 Required Dependencies: None
 Optional Dependencies: None
 Version 0.1
