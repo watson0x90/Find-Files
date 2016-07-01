cac3eb54ea618be92ab7777ad9882e9314882139
Watson <watson0x90@gmail.com>
2016-07-01 12:00:26 -0600

    Create folderacls.ps1

---
+++ folderacls.ps1
@@ -0,0 +1,38 @@
+function Get-FolderACL 
+{
+    [CmdletBinding()]
+	
+    param(
+        [Parameter(Mandatory = $true, position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Path
+		
+    )
+
+    $PathsToSearch = (Get-ChildItem $Path |
+        Where-Object -FilterScript {
+            $_.PSIsContainer 
+        } |
+    Select-Object -ExpandProperty FullName)
+
+    $weakacllist = @()
+    
+    foreach($folder in $PathsToSearch)
+    {
+        $AccessList = ((Get-Item $folder).GetAccessControl('Access')).Access
+        foreach($permission in $AccessList)
+        {
+            if($permission.IdentityReference -like '*domain users*' -or $permission.IdentityReference -like '*everyone*')
+            {
+                $aclObj = New-Object -TypeName System.Object
+                $aclObj | Add-Member -MemberType NoteProperty -Name Path -Value $folder
+                $aclObj | Add-Member -MemberType NoteProperty -Name GroupAccess -Value $permission.IdentityReference
+                $aclObj | Add-Member -MemberType NoteProperty -Name FileSystemRights -Value $permission.FileSystemRights
+                $weakacllist += $aclObj
+
+            }
+        }
+    }
+
+    $weakacllist | Format-List
+}
